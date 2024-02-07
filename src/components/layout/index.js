90199b4d1b6bb27e603a900737ffa6e0cb821663
Agbugba David C. <dagbugba@yahoo.com>
2024-02-07 11:54:56 +0100

    landing page components

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,1 +1,7 @@
-export { default as Nav } from "./navigation/Nav";
+export { default as Nav } from "./landing-page/navigation/Nav";
+export { default as Hero } from "./landing-page/hero/Hero";
+export { default as Footer } from "./landing-page/footer/Footer";
+export { default as Features } from "./landing-page/features/Features";
+export { default as Faq } from "./landing-page/faq/Faq";
+export { default as HowItWorks } from "./landing-page/how_it_works/HowItWorks";
+export { default as Testimonial } from "./landing-page/testimonial/Testimonial";
---
+++ src/components/layout/landing-page/hero/Hero.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { INDEX } from "../../../../routes/CONSTANT";
+
+function Hero() {
+  return (
+    <section className="hero flex items-center justify-center h-screen">
+      <div className="hero-content my-auto">
+        <h6 className="hero-title pb-5">Welcome to Media Space</h6>
+        <h5 className="hero-subtitle text-center pb-3">
+          <span className="spotlight">Elevate</span> Your Communication <br />
+          Experience
+        </h5>
+        <h5 className="hero-lead pt-5 pb-8">
+          Empower Your Organization with Seamless Integration <br /> and
+          Customized Communication Spaces!
+        </h5>
+
+        <div className="flex justify-center">
+          <Link to={INDEX} className="flex hero-btn">
+            Get started
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
