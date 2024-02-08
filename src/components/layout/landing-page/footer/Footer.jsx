accb8c5c0926b170724e5670c00df04d19b21a24
Agbugba David C. <dagbugba@yahoo.com>
2024-02-08 11:51:33 +0100

    footer section adjustment

--- src/components/layout/landing-page/footer/Footer.jsx
+++ src/components/layout/landing-page/footer/Footer.jsx
@@ -7,25 +7,23 @@ function Footer() {
   return (
     <section className="footer w-full">
       <div className="pt-[64px] pb-[64px]">
-        <div className="container">
-          <h1 className="footer-head">
-            Get started for free, <br /> and create a new account
-          </h1>
-          <p className="footer-trial mt-5">
-            Start your 7-day free trial. Cancel anytime.
-          </p>
-          <div className="flex justify-center gap-4 mt-10 mb-12">
-            <Link to={""} className="footer-signin">
-              Sign In
-            </Link>
-            <Link to={""} className="footer-signup">
-              Create Account
-            </Link>
-          </div>
+        <h1 className="footer-head text-center">
+          Get started for free, <br /> and create a new account
+        </h1>
+        <p className="footer-trial mt-5">
+          Start your 7-day free trial. Cancel anytime.
+        </p>
+        <div className="flex justify-center gap-4 mt-10 mb-12">
+          <Link to={""} className="footer-signin">
+            Sign In
+          </Link>
+          <Link to={""} className="footer-signup">
+            Create Account
+          </Link>
+        </div>
 
-          <div className="flex item-center">
-            <div className="footer-divider w-full"></div>
-          </div>
+        <div className="flex text-center">
+          <div className="footer-divider w-full"></div>
         </div>
 
         <div className="flex flex-row justify-evenly gap-10 pt-10">
