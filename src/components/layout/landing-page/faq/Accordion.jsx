dcb73e7a49940e982103488d03db88bf3f0ff83b
Agbugba David C. <dagbugba@yahoo.com>
2024-02-09 12:08:12 +0100

    faq section completed

---
+++ src/components/layout/landing-page/faq/Accordion.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
+import { MdExpandMore, MdExpandLess } from "react-icons/md";
+
+const Accordion = ({ items }) => {
+  const [isOpen, setIsOpen] = useState(null);
+
+  const toggleAccordion = (index) => {
+    if (index === isOpen) {
+      setIsOpen(null);
+    } else {
+      setIsOpen(index);
+    }
+  };
+
+  return (
+    <div>
+      {items.map((item, index) => (
+        <div key={index} className="border rounded-lg p-4 mb-4">
+          <div
+            className="flex justify-between items-center cursor-pointer"
+            onClick={() => toggleAccordion(index)}
+          >
+            <h2 className="faq-accordion">{item.question}</h2>
+            {isOpen === index ? (
+              <MdExpandLess size={20} />
+            ) : (
+              <MdExpandMore size={20} />
+            )}
+          </div>
+          {isOpen === index && <p className="answer mt-3">{item.answer}</p>}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+// Prop validation
+Accordion.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      question: PropTypes.string.isRequired,
+      answer: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Accordion;
---
+++ src/components/layout/landing-page/faq/questions.js
@@ -0,0 +1,65 @@
+export const questions = [
+  {
+    question:
+      "How do I integrate Media Space into my existing apps and workflows?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question:
+      "Can I customize the appearance of my communication space with Media Space?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question:
+      "Is Media Space compatible with both desktop and mobile platforms?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question:
+      "What security measures does Media Space have in place to protect our data?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question: "How does real-time collaboration work within Media Space?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question:
+      "Can I invite external partners or clients to join our Media Space communication hub?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question:
+      "Are there any limits to the number of users that can be added to Media Space?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+  {
+    question:
+      "Is there a trial period available for testing Media Space before committing?",
+    answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.`,
+  },
+];
