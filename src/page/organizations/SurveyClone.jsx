5415b3b6b0222be018068ff908c25e2e367d0196
Agbugba David C. <dagbugba@yahoo.com>
2024-05-20 13:11:29 +0100

    added delete fn to options & questions

--- src/page/organizations/SurveyClone.jsx
+++ src/page/organizations/SurveyClone.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { BiGlobe, BiLock, BiGroup } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
-import { TiDelete } from "react-icons/ti";
 import {
   useCreateSurveyMutation,
   useGetAllSurveyQuery,
@@ -16,15 +15,22 @@ import rtkMutation from "../../utils/rtkMutation";
 import { Table, Badge, Tabs } from "flowbite-react";
 import { ShimmerThumbnail } from "react-shimmer-effects";
 import { CgTrash } from "react-icons/cg";
+import { RiFileEditLine } from "react-icons/ri";
 import { MdOutlineMobileFriendly } from "react-icons/md";
+import Modal from "../../components/modals/Modal";
+import SurveyResponse from "../../components/survey/SurveyResponse";
+import { useGetResponseQuery } from "../../service/admin/surveyResponse.service";
 import { useNavigate } from "react-router-dom";
 
 function Survey() {
+  const [openModal, setOpenModal] = useState(false);
   const [audience, setAudience] = useState("Public");
   const [topic, setTopic] = useState("");
   const [description, setDescription] = useState("");
   const [questions, setQuestions] = useState([]);
-
+  const [editingSurvey, setEditingSurvey] = useState(null);
+  const [showResponse, setShowResponse] = useState(false);
+  const [responseId, setResponseId] = useState("");
   const {
     data: surveyData,
     isLoading: surveyLoading,
@@ -43,6 +49,9 @@ function Survey() {
   const [endSurvey, { isSuccess: endSurveySuccess, error: endSurveyError }] =
     useEndSurveyMutation();
 
+  // const { data: surveyResponse, isLoading: surveyResponseloading } =
+  //   useSurveyResponsesQuery();
+
   const handleEndSurvey = async (id) => {
     const confirmDelete = window.confirm(
       "Are you sure you want to end this survey?"
@@ -60,6 +69,10 @@ function Survey() {
   const navigate = useNavigate();
   const showSurveyResponse = (id) => {
     navigate(`/dashboard/survey/survey-response/${id}`, { state: { id } });
+  };
+
+  const closeResponse = async () => {
+    setShowResponse(false);
   };
 
   useEffect(() => {
@@ -142,19 +155,6 @@ function Survey() {
     setQuestions(newQuestions);
   };
 
-  const handleDeleteQuestion = (index) => {
-    const newQuestions = questions.filter((_, qIndex) => qIndex !== index);
-    setQuestions(newQuestions);
-  };
-
-  const handleDeleteOption = (questionIndex, optionIndex) => {
-    const newQuestions = [...questions];
-    newQuestions[questionIndex].answer_options = newQuestions[
-      questionIndex
-    ].answer_options.filter((_, oIndex) => oIndex !== optionIndex);
-    setQuestions(newQuestions);
-  };
-
   const [createSurvey, { error, isSuccess, isLoading }] =
     useCreateSurveyMutation({
       provideTag: ["Survey"],
@@ -180,6 +180,12 @@ function Survey() {
       showAlert("Oops", error.data.message || "An error occurred", "error");
     }
   }, [isSuccess, error, refetch, activeSurveyRefetch]);
+
+  const handleEditSurvey = (survey) => {
+    setEditingSurvey(survey);
+    setOpenModal(true);
+    console.log(survey);
+  };
 
   return (
     <>
@@ -235,28 +241,16 @@ function Survey() {
 
               {questions.map((question, index) => (
                 <div key={index} className="pb-2">
-                  <div className="flex justify-between items-center">
-                    <div>
-                      <label htmlFor={`question_${index}`}>
-                        Ask a Question:
-                      </label>
-                      <input
-                        type="text"
-                        name={`question_${index}`}
-                        value={question.question_text}
-                        onChange={(e) =>
-                          handleQuestionTextChange(index, e.target.value)
-                        }
-                        className="w-full rounded-md focus:ring-0 focus:outline-none"
-                      />
-                    </div>
-                    <button
-                      onClick={() => handleDeleteQuestion(index)}
-                      className="text-red-500"
-                    >
-                      <TiDelete size={25} />
-                    </button>
-                  </div>
+                  <label htmlFor={`question_${index}`}>Ask a Question:</label>
+                  <input
+                    type="text"
+                    name={`question_${index}`}
+                    value={question.question_text}
+                    onChange={(e) =>
+                      handleQuestionTextChange(index, e.target.value)
+                    }
+                    className="w-full rounded-md focus:ring-0 focus:outline-none"
+                  />
                   <label htmlFor={`answerType_${index}`}>Answer Type:</label>
                   <select
                     id={`answerType_${index}`}
@@ -281,7 +275,7 @@ function Survey() {
                           key={optionIndex}
                           className="flex gap-2 items-center mb-2"
                         >
-                          {/* <input
+                          <input
                             type={
                               question.answer_type === "multiple_choice"
                                 ? "checkbox"
@@ -294,7 +288,7 @@ function Survey() {
                                 e.target.value;
                               setQuestions(newQuestions);
                             }}
-                          /> */}
+                          />
                           <input
                             type="text"
                             value={option}
@@ -306,14 +300,6 @@ function Survey() {
                             }}
                             className="rounded-md focus:ring-0 focus:outline-none"
                           />
-                          <button
-                            onClick={() =>
-                              handleDeleteOption(index, optionIndex)
-                            }
-                            className="text-red-500"
-                          >
-                            <TiDelete size={25} />
-                          </button>
                         </div>
                       ))}
                       <button
@@ -560,6 +546,232 @@ function Survey() {
           </Tabs.Item>
         </Tabs>
       </div>
+
+      <Modal
+        openModal={openModal}
+        modalSize="3xl"
+        onClose={() => setOpenModal(false)}
+        title="Edit Survey"
+      >
+        <>
+          {editingSurvey && (
+            <div>
+              <div className="flex flex-col justify-start mb-5 gap-5">
+                <div className="flex justify-start items-center gap-5">
+                  <label htmlFor="audienceSelect">Select Audience:</label>
+                  {/* Render select options with existing survey data */}
+                  <div className="select-image">
+                    {editingSurvey.audience === "Public" ? (
+                      <BiGlobe size={20} />
+                    ) : editingSurvey.audience === "Private" ? (
+                      <BiLock size={20} />
+                    ) : (
+                      <BiGroup size={20} />
+                    )}
+                  </div>
+                  <select
+                    value={editingSurvey.audience}
+                    // Handle onChange event
+                    onChange={(e) => {
+                      // Update editingSurvey state
+                      setEditingSurvey({
+                        ...editingSurvey,
+                        audience: e.target.value,
+                      });
+                    }}
+                    className="focus:outline-none focus:ring-0 border-0 bg-transparent"
+                  >
+                    <option value="Public">Public</option>
+                    <option value="Private">Private</option>
+                    <option value="Followers">Followers</option>
+                  </select>
+                </div>
+                {/* Render other input fields with existing survey data */}
+                <div>
+                  <label htmlFor="topic">Survey Topic:</label>
+                  <input
+                    type="text"
+                    id="topic"
+                    value={editingSurvey.topic}
+                    // Handle onChange event
+                    onChange={(e) => {
+                      // Update editingSurvey state
+                      setEditingSurvey({
+                        ...editingSurvey,
+                        topic: e.target.value,
+                      });
+                    }}
+                    className="w-full rounded-md focus:ring-0 focus:outline-none"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="description">Description:</label>
+                  <textarea
+                    id="description"
+                    value={editingSurvey.description}
+                    // Handle onChange event
+                    onChange={(e) => {
+                      // Update editingSurvey state
+                      setEditingSurvey({
+                        ...editingSurvey,
+                        description: e.target.value,
+                      });
+                    }}
+                    className="w-full rounded-md focus:ring-0 focus:outline-none"
+                  />
+                </div>
+              </div>
+              {/* Render existing questions with their data */}
+              {editingSurvey.questions.map((question, index) => (
+                <div key={index} className="pb-2">
+                  {/* Render question input with existing data */}
+                  <label htmlFor={`question_${index}`}>Ask a Question:</label>
+                  <input
+                    type="text"
+                    name={`question_${index}`}
+                    value={question.question_text}
+                    // Handle onChange event
+                    onChange={(e) => {
+                      // Update editingSurvey state
+                      const newQuestions = [...editingSurvey.questions];
+                      newQuestions[index].question_text = e.target.value;
+                      setEditingSurvey({
+                        ...editingSurvey,
+                        questions: newQuestions,
+                      });
+                    }}
+                    className="w-full rounded-md focus:ring-0 focus:outline-none"
+                  />
+                  {/* Render answer type select with existing data */}
+                  <label htmlFor={`answerType_${index}`}>Answer Type:</label>
+                  <select
+                    id={`answerType_${index}`}
+                    value={question.answer_type}
+                    // Handle onChange event
+                    onChange={(e) => {
+                      // Update editingSurvey state
+                      const newQuestions = [...editingSurvey.questions];
+                      newQuestions[index].answer_type = e.target.value;
+                      setEditingSurvey({
+                        ...editingSurvey,
+                        questions: newQuestions,
+                      });
+                    }}
+                    className="focus:outline-none focus:ring-0 border-0 bg-transparent"
+                  >
+                    <option value="multiple_choice">Multiple Choice</option>
+                    <option value="single_choice">Single Choice</option>
+                    <option value="text">Text</option>
+                    <option value="true_or_false">True/False</option>
+                    <option value="time">Time</option>
+                    <option value="date">Date</option>
+                  </select>
+
+                  {(question.answer_type === "multiple_choice" ||
+                    question.answer_type === "single_choice") && (
+                    <div>
+                      {question.answer_options.map((option, optionIndex) => (
+                        <div
+                          key={optionIndex}
+                          className="flex gap-2 items-center mb-2"
+                        >
+                          <input
+                            type={
+                              question.answer_type === "multiple_choice"
+                                ? "checkbox"
+                                : "radio"
+                            }
+                            checked={question.answer_options.includes(option)}
+                            onChange={(e) => {
+                              const newQuestions = [...editingSurvey.questions];
+                              // If it's a multiple choice question
+                              if (question.answer_type === "multiple_choice") {
+                                // Toggle the option in the array based on whether it's already selected or not
+                                if (e.target.checked) {
+                                  newQuestions[index].answer_options.push(
+                                    option
+                                  );
+                                } else {
+                                  const optionIndex =
+                                    newQuestions[index].answer_options.indexOf(
+                                      option
+                                    );
+                                  if (optionIndex !== -1) {
+                                    newQuestions[index].answer_options.splice(
+                                      optionIndex,
+                                      1
+                                    );
+                                  }
+                                }
+                              } else {
+                                // For single choice, replace the options array with the selected option
+                                newQuestions[index].answer_options = [option];
+                              }
+                              setEditingSurvey({
+                                ...editingSurvey,
+                                questions: newQuestions,
+                              });
+                            }}
+                          />
+                          <input
+                            type="text"
+                            value={option}
+                            onChange={(e) => {
+                              const newQuestions = [...editingSurvey.questions];
+                              newQuestions[index].answer_options[optionIndex] =
+                                e.target.value;
+                              setEditingSurvey({
+                                ...editingSurvey,
+                                questions: newQuestions,
+                              });
+                            }}
+                            className="rounded-md focus:ring-0 focus:outline-none"
+                          />
+                        </div>
+                      ))}
+
+                      <button
+                        onClick={() => {
+                          const newQuestions = [...editingSurvey.questions];
+                          newQuestions[index].answer_options.push("");
+                          setEditingSurvey({
+                            ...editingSurvey,
+                            questions: newQuestions,
+                          });
+                        }}
+                        className="border p-1 rounded-md flex items-center gap-1 mt-3"
+                      >
+                        <IoMdAdd size={20} />
+                        Add Option
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+              {/* Add more code for handling adding/removing questions */}
+              {/* ... */}
+              <div className="survey-box flex justify-end pt-5">
+                <button
+                  onClick={handleSubmit}
+                  className="text-sm w-full text-white bg-[#34B53A] p-2 border rounded-md"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          )}
+        </>
+      </Modal>
+      {/* {showResponse && responseId && (
+        <Modal
+          title="Survey Responses"
+          openModal={showResponse}
+          modalSize="4xl"
+          onClose={() => setShowResponse(false)}
+        >
+          <SurveyResponse id={responseId} onclose={closeResponse} />
+        </Modal>
+      )} */}
     </>
   );
 }
