70384a03c1a054287f43e5b01f4bb37fc1c1f1f6
Agbugba David C. <57929456+Davidruph@users.noreply.github.com>
2024-06-27 15:02:32 +0100

    word cloud, net sentiment done

--- src/page/sentiment-analysis/Analysis.jsx
+++ src/page/sentiment-analysis/Analysis.jsx
@@ -8,7 +8,14 @@ import post from "../../assets/icons/post.svg";
 import positive from "../../assets/icons/positive.svg";
 import negative from "../../assets/icons/negative.svg";
 import DynamicLineChart from "../../components/charts/DynamicLineChart";
-import data from "./chart-data"; // Assuming this data is an array of objects
+import data from "./chart-data";
+import words from "./words";
+import ReactWordcloud from "react-wordcloud";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+import GaugeComponent from "react-gauge-component";
+import Legend from "./Legend";
+import arrow from "./arrow.svg";
 
 const groupDataByMonth = (data, yKeys) => {
   const groupedData = {};
@@ -53,6 +60,24 @@ const Analysis = () => {
   const yKeys = ["positive", "negative", "neutral"];
   const groupedData = groupDataByMonth(data, yKeys);
   const flattenedData = flattenGroupedData(groupedData);
+  const callbacks = {
+    getWordColor: (word) => (word.value > 40 ? "teal" : "red"),
+    onWordClick: console.log,
+    onWordMouseOver: console.log,
+    getWordTooltip: (word) =>
+      `${word.text} (${word.value}) [${word.value > 50 ? "good" : "bad"}]`,
+  };
+  const options = {
+    rotations: 2,
+    rotationAngles: [0, 0],
+  };
+  const size = [614, 350];
+
+  const legendItems = [
+    { color: "#FF3A29", label: "Negative" },
+    { color: "#D4BD52", label: "Neutral" },
+    { color: "#4E9C19", label: "Positive" },
+  ];
 
   return (
     <>
@@ -82,7 +107,7 @@ const Analysis = () => {
                 </select>
               </div>
 
-              <div className="w-full super-admin-card-box border-[#E6EDFF]  items-center justify-center sm:flex-row sm:flex-wrap sm:justify-center border-0 shadow sm:gap-10 md:grid md:grid-cols-2 md:justify-between lg:grid lg:grid-cols-4 xl:grid-cols-4 mb-20">
+              <div className="w-full super-admin-card-box border-[#E6EDFF]  items-center justify-center sm:flex-row sm:flex-wrap sm:justify-center border shadow sm:gap-10 md:grid md:grid-cols-2 md:justify-between lg:grid lg:grid-cols-4 xl:grid-cols-4 mb-20">
                 {loadStats ? (
                   <ShimmerThumbnail width={250} height={150} />
                 ) : (
@@ -127,7 +152,7 @@ const Analysis = () => {
           ) : null}
 
           <div className="flex justify-between gap-10 pb-10">
-            <div className="wordcloud w-full h-[449px] rounded-[13.17px] shadow border-[#E6EDFF] bg-white">
+            <div className="wordcloud w-full h-[449px] rounded-[13.17px] border shadow border-[#E6EDFF] bg-white">
               <div className="px-10 pt-3">
                 <div className="flex justify-between items-center">
                   <p className="word-cloud-text">Word Cloud</p>
@@ -135,9 +160,17 @@ const Analysis = () => {
                     <option value="This Week">This Week</option>
                   </select>
                 </div>
+                <div className="flex justify-center items-center w-full">
+                  <ReactWordcloud
+                    callbacks={callbacks}
+                    options={options}
+                    size={size}
+                    words={words}
+                  />
+                </div>
               </div>
             </div>
-            <div className="net-sentiment w-full max-w-[376px] h-[449px]  rounded-[13.17px] shadow border-[#E6EDFF] bg-white">
+            <div className="net-sentiment w-full max-w-[376px] h-[449px]  rounded-[13.17px] border shadow border-[#E6EDFF] bg-white">
               <div className="p-3">
                 <div className="flex items-center justify-between">
                   <p className="word-cloud-text">Net Sentiment</p>
@@ -145,25 +178,70 @@ const Analysis = () => {
                     <option value="Today">Today</option>
                   </select>
                 </div>
+
+                <div className="flex flex-col justify-center mt-8">
+                  <GaugeComponent
+                    value={80}
+                    type="semicircle"
+                    labels={{
+                      tickLabels: {
+                        type: "inner",
+                        ticks: [
+                          { value: 20 },
+                          { value: 40 },
+                          { value: 60 },
+                          { value: 80 },
+                          { value: 100 },
+                        ],
+                      },
+                      valueLabel: {
+                        hide: true,
+                      },
+                    }}
+                    arc={{
+                      colorArray: ["#EA4228", "#5BE12C"],
+                      padding: 0.02,
+                      width: 0.3,
+                      nbSubArcs: 0,
+                    }}
+                    pointer={{
+                      elastic: true,
+                      animationDelay: 0,
+                      color: "#272525",
+                      width: 10,
+                      length: 0.8,
+                    }}
+                  />
+
+                  <div className="flex flex-col w-full justify-center items-center mb-40">
+                    <h1 className="guage-value-head">+80</h1>
+                    <p className="guage-rating">VERY POSITIVE</p>
+                    <p className="guage-period flex items-center gap-1 py-2">
+                      <img src={arrow} alt="" />
+                      7.2%
+                    </p>
+                    <p className="guage-period">Previous period: 74.24</p>
+                  </div>
+
+                  <Legend items={legendItems} />
+                </div>
               </div>
             </div>
           </div>
 
           <div className="flex gap-10 justify-between">
-            <div className="flex w-full rounded-[13.17px] border-[#E6EDFF] bg-white h-[537.02px] shadow">
+            <div className="flex w-full border rounded-[13.17px] border-[#E6EDFF] bg-white h-[537.02px] shadow">
               <DynamicLineChart
                 data={flattenedData}
                 xKey="date"
                 yKeys={yKeys} // Pass the yKeys array
               />
             </div>
-            <div className="net-sentiment w-full max-w-[376px] h-[537.02px]  rounded-[13.17px] shadow border-[#E6EDFF] bg-white">
+            <div className="net-sentiment w-full max-w-[376px] h-[537.02px] border  rounded-[13.17px] shadow border-[#E6EDFF] bg-white">
               <div className="p-3">
                 <div className="flex items-center justify-between">
-                  <p className="word-cloud-text">Net Sentiment</p>
-                  <select className="w-[106.97px] h-[23.21px] analysis-filter-input focus:outline-none focus:ring-0">
-                    <option value="Today">Today</option>
-                  </select>
+                  <p className="word-cloud-text">Trending keywords</p>
+                  <p className="sentiment">Sentiment</p>
                 </div>
               </div>
             </div>
