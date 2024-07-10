ef4036ed2a7d91251fd29eb86d7f0de9514b1dff
Agbugba David C. <57929456+Davidruph@users.noreply.github.com>
2024-07-10 14:57:53 +0100

    landing page modification

--- src/components/layout/landing-page/hero/Hero.jsx
+++ src/components/layout/landing-page/hero/Hero.jsx
@@ -1,29 +1,60 @@
 import { Link } from "react-router-dom";
 import { INDEX } from "../../../../routes/CONSTANT";
+import imagehero from "../../../../assets/imagehero.svg";
+import { REGISTER } from "../../../../routes/CONSTANT";
 
 function Hero() {
   return (
-    <section className="hero bg-[#F7FEEE] h-screen flex justify-center items-center">
-      <div className="container">
-        <div className="hero-content text-center">
-          <h6 className="hero-title pb-5">Welcome to Media Space</h6>
-          <h5 className="hero-subtitle pb-3">
-            <span className="spotlight">Elevate</span> Your Communication <br />
-            Experience
-          </h5>
-          <h5 className="hero-lead pt-5 pb-8">
-            Empower Your Organization with Seamless Integration <br /> and
-            Customized Communication Spaces!
-          </h5>
+    <>
+      <section className="h-[558px] heroimg w-full">
+        <div className="w-full h-full flex justify-center items-center px-3">
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="first-hero-head hero-subtitle text-[#7ADD02] text-center">
+              Elevate{" "}
+              <span className="text-[#FBFBFB]">
+                Your <br /> Communication Experience
+              </span>
+            </h3>
+            <p className="first-hero-subtitle text-[#FBFBFB] pt-6 pb-14">
+              Empower Your Organization with Seamless Integration <br /> and
+              Customized Communication Spaces!
+            </p>
 
-          <div className="flex justify-center">
-            <Link to={INDEX} className="hero-btn">
-              Get started
-            </Link>
+            <div className="flex gap-5">
+              <Link to={REGISTER} className="flex action-button sm:mr-1">
+                Get started
+              </Link>
+              <Link
+                to={REGISTER}
+                className="flex action-button border border-[#3D7100] bg-transparent text-white sm:mr-1"
+              >
+                learn more
+              </Link>
+            </div>
+          </div>
+          <img src={imagehero} className="hidden lg:flex" alt="" />
+        </div>
+      </section>
+      <section className="hero bg-[#F7FEEE] h-screen flex justify-center items-center">
+        <div className="container">
+          <div className="hero-content text-center">
+            <h5 className="hero-subtitle pb-3">
+              <span className="spotlight">Share</span> a Community with <br />
+              People around the World
+            </h5>
+            <h5 className="hero-lead pt-5 pb-8">
+              Do more with Kommunita and grow your <br /> organization’s reach
+            </h5>
+
+            <div className="flex justify-center">
+              <Link to={INDEX} className="hero-btn">
+                Get started
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
